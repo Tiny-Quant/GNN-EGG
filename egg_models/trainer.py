eba3bec43b7812dc3599da434a9fbfe6ba77f38c
Art Tay <s224833@NucleusC025.cm.cluster>
2024-01-04 11:46:59 -0600

    See last commit.

---
+++ egg_models/trainer.py
@@ -0,0 +1,60 @@
+# %% Dependencies:
+import torch 
+import torch.nn as nn
+
+from utils.ceograph import NucleiData
+
+class Trainer:
+    def __init__(self, 
+                 generator: EGG, 
+                 explainee: nn.Module, 
+                 optimizer: torch.optim.Optimizer, 
+                 criterion: callable) -> None:
+        self.generator = generator
+        self.explainee = explainee
+        self.criterion = criterion
+        self.optimizer = optimizer
+
+    def train(self, target, obs, num_epochs, 
+              lambda_1=1, lambda_2=1, lambda_3=1):
+        for epoch in range(num_epochs):
+
+            self.optimizer.zero_grad()
+            # X[b, nodes, feat]; C_x[b, nodes]; A[b, 2, edges];
+            # E[b, edges, feat]; lik[b]
+            X, C_x, A, E, C_x_logLik, A_logLik = self.generator()
+
+            graph_list = [
+                NucleiData(X, C_x, A, E) for (X, C_x, A, E) in 
+                zip(X.unbind(), C_x.unbind(), A.unbind(), E.unbind())
+            ]
+
+            graph_list = [
+                clear_iso_nodes(graph) for graph in graph_list
+            ]
+
+            pred_loss_fn = PredLoss(target, self.criterion, self.explainee)
+            pred_losses = pred_loss_fn(graph_list) 
+            pred_rewards = 1 / pred_losses + 1e-4 # avoid zero division.
+            pred_loss = (pred_losses.sum() + 
+                    pred_rewards @ -C_x_logLik + 
+                    pred_rewards @ -A_logLik) / self.generator.batch_size
+
+            edit_loss_fn = EditLoss()
+            edit_dists = edit_loss_fn(graph_list, obs) / len(obs)
+            edit_rewards = 1 / edit_dists + 1 # smoother. 
+            edit_loss = (edit_rewards @ -C_x_logLik.repeat(len(obs)) + 
+                         edit_rewards @ -A_logLik.repeat(len(obs)))
+
+            edge_pen = torch.norm(self.generator.AdjacencyMatrix.edge_logits, 
+                                  p=1)
+
+            loss = (lambda_1 * pred_loss +
+                    lambda_2 * edit_loss + 
+                    lambda_3 * edge_pen)
+
+            loss.backward()
+            self.optimizer.step()
+
+            print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss:.4f} " + 
+                f"Time: {end-start:.2f} seconds")
