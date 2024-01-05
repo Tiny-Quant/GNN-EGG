532cbf122188c2c7d4ddf03eb205ae8b7f614c8e
Art Tay <s224833@NucleusC007.cm.cluster>
2024-01-04 19:25:25 -0600

    Config file and cli parsing.

--- scripts/train_egg_hard_nx.py
+++ scripts/train_egg_hard_nx.py
@@ -2,8 +2,7 @@
 
 import torch
 import torch.nn as nn 
-import torch.optim
-torch.autograd.set_detect_anomaly(True)
+# torch.autograd.set_detect_anomaly(True)
 
 # Adds the repo directory to the import paths.
 import sys
@@ -11,6 +10,8 @@ from os.path import dirname, abspath
 repo_dir = dirname(dirname(abspath(__file__)))
 sys.path.append(repo_dir)
 import pickle
+import argparse
+import json
 
 from egg_models.egg_hard_nx import EggHardNx
 from egg_models.losses import PredLoss
@@ -19,20 +20,42 @@ from utils import ceograph
 
 # %%
 if __name__ == '__main__':
+    
+    # Parse config.
+    parser = argparse.ArgumentParser() 
+    parser.add_argument(
+        # from repo dir
+        '--path_to_json_config', 
+        type=str, 
+        default="config.json"
+    )
+    opt = parser.parse_args()
 
-    MAX_NODES = 50
-    CONT_NODE_FEATS = 11
-    CELL_TYPES = 5
-    CONT_EDGE_FEAT = 2
-    BATCH_SIZE = 10
+    config_path = repo_dir + "/" + opt.path_to_json_config
+    with open(config_path) as f:
+        config_data = json.load(f)
+
+    num_epochs = config_data["num_epochs"]
+    learning_rate = config_data["learning_rate"]
+    optimizer_name = config_data["optimizer_name"]
+    batch_size = config_data["batch_size"]
+    max_nodes = config_data["max_nodes"]
+    target_name = config_data["target_name"]
+    obs_path = config_data["obs_path"]
 
     device = torch.device(0)
 
-    target = torch.tensor([1.0, 0.0]).to(device)
-    path = repo_dir + "/" + "data/slides/LUDA/ad_train_nx_100.pkl" 
-    with open(path, 'rb') as f:
+    if target_name == "ad":
+        target = torch.tensor([1.0, 0.0]).to(device)
+    else: 
+        target = torch.tensor([0.0, 1.0]).to(device)
+
+    with open(obs_path, 'rb') as f:
         obs = pickle.load(f)
 
+    CONT_NODE_FEATS = 11
+    CELL_TYPES = 5
+    CONT_EDGE_FEAT = 2
     explainee = ceograph.NucleiNet(CONT_NODE_FEATS, CONT_EDGE_FEAT, batch=False)
     explainee.to(device)
     explainee.load_state_dict(torch.load(
@@ -43,16 +66,24 @@ if __name__ == '__main__':
     explainee.eval()
 
     generator = EggHardNx(
-        node_size=MAX_NODES, cont_node_feat=CONT_NODE_FEATS, 
+        node_size=max_nodes, cont_node_feat=CONT_NODE_FEATS, 
         node_types=CELL_TYPES, cont_edge_feat=CONT_EDGE_FEAT, 
-        batch_size=BATCH_SIZE, 
+        batch_size=batch_size, 
     )
-    generator.to(device)
+    generator.to(device) 
+    generator.train()
 
-    optimizer = torch.optim.Adam(generator.parameters())    
+    # Dynamically create the optimizer class
+    optimizer_class_str = "optim." + optimizer_name
+    optimizer_class = getattr(torch.optim, optimizer_name, None)
+
+    if optimizer_class is not None:
+        optimizer = optimizer_class(generator.parameters(), lr=learning_rate)
+    else:
+        raise ValueError(f"Unsupported optimizer: {optimizer_name}")
 
     criterion = nn.BCELoss()
 
     trainer = Trainer(generator, explainee, optimizer, criterion, target, obs)
 
-    trainer.train()
+    trainer.train(num_epochs=num_epochs)
