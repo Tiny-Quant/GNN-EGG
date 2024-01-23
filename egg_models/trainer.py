fe12a69dda52be397dc7559049ef6d490ef39a6e
Art Tay <s224833@NucleusC023.cm.cluster>
2024-01-23 10:01:11 -0600

    Added BaseTrainer class.

--- egg_models/trainer.py
+++ egg_models/trainer.py
@@ -1,5 +1,6 @@
 # %% Dependencies:
 import math 
+from abc import abstractmethod
 
 import torch 
 import torch.nn as nn
@@ -144,3 +145,67 @@ class Trainer:
                 }
                 checkpoint_filename = f'{self.checkpoint_path}/checkpoint_epoch_{epoch}.pt'
                 torch.save(checkpoint, checkpoint_filename)
+
+class BaseTrainer:
+    def __init__(self, model: nn.Module, optimizer: torch.optim.Optimizer, 
+                checkpoint_path, save_every=1):
+        self.model = model
+        self.optimizer = optimizer
+        self.checkpoint_path = checkpoint_path
+        self.save_every = save_every
+    
+    @abstractmethod
+    def train_one_epoch(self):
+        raise NotImplementedError
+
+    def per_epoch_logger(self):
+        pass
+    
+    def save_checkpoint(self, epoch, total_epochs):
+
+        if epoch % self.save_every == 0 or epoch == total_epochs: 
+            checkpoint = {
+                'epoch': epoch,
+                'generator_state_dict': self.model.state_dict(),
+                'optimizer_state_dict': self.optimizer.state_dict(),
+            }
+            checkpoint_filename = f'{self.checkpoint_path}/checkpoint_epoch_{epoch}.pt'
+            torch.save(checkpoint, checkpoint_filename)
+
+    def train(self, num_epochs=1, resume_path=None, profile_dir=None):
+
+        if resume_path is not None:
+            last_checkpoint = torch.load(resume_path)
+            self.mode.load_state_dict(
+                last_checkpoint['model_state_dict']
+            )
+            self.optimizer.load_state_dict(
+                last_checkpoint['optimizer_state_dict']
+            )
+            start_epoch = last_checkpoint['epoch'] + 1
+        else: 
+            start_epoch = 0
+
+        total_epochs = start_epoch + num_epochs 
+        for epoch in tqdm(range(start_epoch, total_epochs), desc="Training"):
+            
+            if profile_dir is not None and epoch == math.ceil((start_epoch + total_epochs) / 2):
+                with profile( 
+                    activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], 
+                    profile_memory=True, with_stack=True,
+                    # ref: https://github.com/pytorch/pytorch/issues/100253
+                    experimental_config=torch._C._profiler._ExperimentalConfig(verbose=True)
+                ) as prof: 
+                    result = self.train_one_epoch()
+                with open(profile_dir + "/" + "profile.txt", "a+") as f:
+                    print(prof.key_averages().table(sort_by="self_cuda_time_total"), 
+                          file=f)
+
+            else: 
+                result = self.train_one_epoch()
+
+            self.per_epoch_logger()
+
+            self.save_checkpoint(epoch, total_epochs)
+
+
