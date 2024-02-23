24df4157b6705c9050decf66a859b4a31dedd8ca
Art Tay <s224833@NucleusC018.cm.cluster>
2024-02-23 11:04:38 -0600

    Passing test 1 and test 2.

--- egg_models/egg_generic.py
+++ egg_models/egg_generic.py
@@ -1,6 +1,6 @@
 # %% Dependencies
 
-from typing import Optional
+from typing import Optional, Tuple
 
 import torch
 import torch.nn as nn 
@@ -18,24 +18,25 @@ from egg_models.generic_layers import (
 class EggGeneric(nn.Module):
     def __init__(self, max_node_size: int, 
                  cont_node_feats: Optional[int] = None, 
-                 dis_node_feats: Optional[tuple[int]] = None, 
+                 dis_node_feats: Optional[Tuple[int]] = None, 
                  cont_edge_feats: Optional[int] = None, 
-                 dis_edge_feats: Optional[tuple[int]] = None,
+                 dis_edge_feats: Optional[Tuple[int]] = None,
                  temp = 1.0, 
                  batch_size=1): 
+
+        super(EggGeneric, self).__init__()
 
         # Parameter Checks: 
         if cont_node_feats is None and dis_node_feats is None:
             raise ValueError("Need at least one of `cont_node_feats` or " +  
                              "`dis_node_feats`")
 
-        super(EggGeneric, self).__init__()
         self.max_node_size = max_node_size
         self.cont_node_feats = cont_node_feats      
         self.dis_node_feats = dis_node_feats
         self.cont_edge_feats = cont_edge_feats
         self.dis_edge_feats = dis_edge_feats
-        self.temps = temp
+        self.temp = temp
         self.batch_size = batch_size
 
         # Layers 
@@ -56,7 +57,7 @@ class EggGeneric(nn.Module):
 
         if self.cont_edge_feats is not None: 
             self.ContEdgeFeats = ContFeatMatrix(
-                self.batch_size, self.node_size**2, self.cont_edge_feat
+                self.batch_size, self.max_node_size**2, self.cont_edge_feats
             )
 
         if self.dis_edge_feats is not None: 
@@ -77,7 +78,17 @@ class EggGeneric(nn.Module):
         self.device_param = nn.Parameter(torch.empty(0))
     
     def forward(self):
-
+        """
+        returns: dict
+            - cont_node_feats: torch.Size([b, n, f_1])
+            - dis_node_feats: torch.Size([b, n, total_cats])
+            - cont_edge_feats: torch.Size([b, e, f_2])
+            - dis_edge_feats: torch.Size([b, n, total_cats])
+            - full_edge_indices: torch.Size([b, 2, e])
+            - C_x_logLik: torch.Size([b, f_3])
+            - C_e_logLik: torch.Size([b, f_4])
+            - A_logLik: torch.Size([b])
+        """
         if self.cont_node_feats is not None:
             X = self.ContNodeFeats()
         else: 
@@ -86,10 +97,10 @@ class EggGeneric(nn.Module):
         if self.dis_node_feats is not None: 
             C_x , C_x_logLik = [], []
             for layer in self.DisNodeFeats:
-                out_C_x, out_C_x_logLik = self.DisNodeFeats()
+                out_C_x, out_C_x_logLik = layer()
                 C_x.append(out_C_x)
                 C_x_logLik.append(out_C_x_logLik)
-            C_x = torch.stack(C_x, dim=1)
+            C_x = torch.cat(C_x, dim=2)
             C_x_logLik = torch.stack(C_x_logLik, dim=1)
         else: 
             C_x, C_x_logLik = None, None
@@ -102,10 +113,10 @@ class EggGeneric(nn.Module):
         if self.dis_edge_feats is not None: 
             C_e , C_e_logLik = [], []
             for layer in self.DisEdgeFeats:
-                out_C_e, out_C_e_logLik = self.DisEdgeFeats()
+                out_C_e, out_C_e_logLik = layer()
                 C_e.append(out_C_e)
                 C_e_logLik.append(out_C_e_logLik)
-            C_e = torch.stack(C_e, dim=1)
+            C_e = torch.cat(C_e, dim=2)
             C_e_logLik = torch.stack(C_e_logLik, dim=1)
         else: 
             C_e, C_e_logLik = None, None
