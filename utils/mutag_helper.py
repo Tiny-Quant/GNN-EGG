a806b7dd1696253f1280e5b8f6fd701acb0ee39c
Art Tay <s224833@NucleusC002.cm.cluster>
2024-04-13 21:24:28 -0500

    Moved explainee model to helper file.

--- utils/mutag_helper.py
+++ utils/mutag_helper.py
@@ -1,12 +1,42 @@
 from typing import List
 
 import torch 
+from torch.nn import Linear
 import torch.nn.functional as F
 
 import torch_geometric as pyg
+from torch_geometric.nn import GCNConv
+from torch_geometric.nn import global_mean_pool
 from torch_geometric.utils import remove_isolated_nodes, remove_self_loops
 
 from pygmtools.utils import dense_to_sparse, build_batch
+
+class GCN(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super(GCN, self).__init__()
+        torch.manual_seed(12345)
+        self.conv1 = GCNConv(7, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, hidden_channels)
+        self.lin = Linear(hidden_channels, 2)
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        # 1. Obtain node embeddings 
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = self.conv3(x, edge_index)
+
+        # 2. Readout layer
+        x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
+
+        # 3. Apply a final classifier
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin(x)
+        
+        return x
 
 def clean_gen_graph(gen: pyg.data.Data) -> pyg.data.Data: 
     """
@@ -41,6 +71,7 @@ def egg_to_ex(generated: dict):
         edge_index = dense_to_sparse(A_hard)[0].transpose(1, 2)
 
     X = generated['dis_node_feats']
+
     E = generated['dis_edge_feats'].narrow(dim=1, start=0, 
                                            length=edge_index.shape[2])
 
