282c54fc6ff59d6afa4cd0f46423fddb8cd9e40c
Tiny-Quant <tay.42@gmail.com>
2025-11-15 12:15:45 -0600

    Ran MUTAG; BA-2MOTIF; PROTEINS.

--- revisions/new_src/criteria.py
+++ revisions/new_src/criteria.py
@@ -55,3 +55,17 @@ class EmbeddingCriterion(nn.Module):
     def forward(self, embeds):
         assert len(embeds.shape) == 2
         return (1 - F.cosine_similarity(self.target[None, :], embeds)).mean()
+
+class KLDivergencePenalty(nn.Module):
+    def __init__(self, binary=True, eps=1e-4):
+        super().__init__()
+        self.binary = binary
+        self.eps = eps
+
+    def forward(self, pq):
+        p = pq[0] * (1 - 2*self.eps) + self.eps
+        q = pq[1] * (1 - 2*self.eps) + self.eps
+        if self.binary:
+            p = torch.stack([p, 1-p], dim=-1)
+            q = torch.stack([q, 1-q], dim=-1)
+        return torch.sum(p * (p / q).log())
